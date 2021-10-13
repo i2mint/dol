c2dd6cc27c06623e44028f0f2b818da0db5810db
Thor Whalen <thorwhalen1@gmail.com>
2021-10-13 11:24:22 -0700

    feat: iSliceStore

---
+++ dol/tools.py
@@ -0,0 +1,93 @@
+"""
+Various tools to add functionality to stores
+"""
+
+from itertools import islice
+from collections.abc import Mapping
+
+
+class iSliceStore(Mapping):
+    """
+    Wraps a store to make a reader that acts as if the store was a list
+    (with integer keys, and that can be sliced).
+    I say "list", but it should be noted that the behavior is more that of range,
+    that outputs an element of the list
+    when keying with an integer, but returns an iterable object (a range) if sliced.
+
+    Here, a map object is returned when the sliceable store is sliced.
+
+    >>> s = {'foo': 'bar', 'hello': 'world', 'alice': 'bob'}
+    >>> sliceable_s = iSliceStore(s)
+
+    The read-only functionalities of the underlying mapping are still available:
+
+    >>> list(sliceable_s)
+    ['foo', 'hello', 'alice']
+    >>> 'hello' in sliceable_s
+    True
+    >>> sliceable_s['hello']
+    'world'
+
+    But now you can get slices as well:
+
+    >>> list(sliceable_s[0:2])
+    ['bar', 'world']
+    >>> list(sliceable_s[-2:])
+    ['world', 'bob']
+    >>> list(sliceable_s[:-1])
+    ['bar', 'world']
+
+    Now, you can't do `sliceable_s[1]` because `1` isn't a valid key.
+    But if you really wanted "item number 1", you can do:
+
+    >>> next(sliceable_s[1:2])
+    'world'
+
+    Note that `sliceable_s[i:j]` is an iterable that needs to be consumed
+    (here, with list) to actually get the data. If you want your data in a different
+    format, you can use `dol.trans.wrap_kvs` for that.
+
+    >>> from dol import wrap_kvs
+    >>> ss = wrap_kvs(sliceable_s, obj_of_data=list)
+    >>> ss[1:3]
+    ['world', 'bob']
+    >>> sss = wrap_kvs(sliceable_s, obj_of_data=sorted)
+    >>> sss[1:3]
+    ['bob', 'world']
+    """
+
+    def __init__(self, store):
+        self.store = store
+
+    def _get_key(self, k: int):
+        return next(islice(self.store.keys(), k, k + 1))
+
+    def _get_keys(self, k: slice):
+        start, stop, step = k.start, k.stop, k.step
+        assert (step is None) or (step > 0), "step of slice can't be negative"
+
+        negative_start = start is not None and start < 0
+        negative_stop = stop is not None and stop < 0
+        if negative_start or negative_stop:
+            n = self.__len__()
+            if negative_start:
+                start = n + start
+            if negative_stop:
+                stop = n + stop
+
+        return islice(self.store.keys(), start, stop, step)
+
+    def __getitem__(self, k):
+        if not isinstance(k, slice):
+            return self.store[k]
+        else:
+            return map(self.store.__getitem__, self._get_keys(k))
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        return len(self.store)
+
+    def __contains__(self, k):
+        return k in self.store
