29e4fbce482a2a1a0f10423e5a42247d2a575182
Thor Whalen <thorwhalen1@gmail.com>
2023-02-01 12:00:27 +0100

    test: test_wrap_kvs_vs_class_and_static_methods

---
+++ dol/tests/test_edge_cases.py
@@ -0,0 +1,42 @@
+from dol import Files, wrap_kvs
+
+
+def test_wrap_kvs_vs_class_and_static_methods():
+    """Adding wrap_kvs breaks classmethods and staticmethods
+
+    https://github.com/i2mint/dol/issues/17
+    """
+    def returnx(x):
+        return x
+
+    @wrap_kvs(data_of_obj=returnx, obj_of_data=returnx)
+    class MyFiles(Files):
+        @classmethod
+        def hello(cls):
+            print('hello')
+
+        @staticmethod
+        def hi():
+            print('hi')
+
+    errors = []
+
+    try:
+        MyFiles.hello()
+    except Exception as e:
+        print('classmethod hello is broken by wrap_kvs decorator')
+        print(f'{type(e).__name__}: {e}')
+        errors.append(e)
+
+
+    try:
+        MyFiles.hi()
+    except Exception as e:
+        print('staticmethod hi is broken by wrap_kvs decorator')
+        print(f'{type(e).__name__}: {e}')
+        errors.append(e)
+
+    if errors:
+        first_error, *_ = errors
+        raise first_error
+
