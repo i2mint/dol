d2c0d93b9804232b45226e862e00b3332f71558b
Thor Whalen <thorwhalen1@gmail.com>
2023-02-01 12:26:25 +0100

    test: updated test_wrap_kvs_vs_class_and_static_methods

--- dol/tests/test_edge_cases.py
+++ dol/tests/test_edge_cases.py
@@ -2,15 +2,21 @@ from dol import Files, wrap_kvs
 
 
 def test_wrap_kvs_vs_class_and_static_methods():
-    """Adding wrap_kvs breaks classmethods and staticmethods
+    """Adding wrap_kvs breaks methods
 
     https://github.com/i2mint/dol/issues/17
     """
+
     def returnx(x):
         return x
 
     @wrap_kvs(data_of_obj=returnx, obj_of_data=returnx)
     class MyFiles(Files):
+        y = 2
+
+        def normal_method(self, x=3):
+            return self.y * x
+
         @classmethod
         def hello(cls):
             print('hello')
@@ -24,10 +30,16 @@ def test_wrap_kvs_vs_class_and_static_methods():
     try:
         MyFiles.hello()
     except Exception as e:
-        print('classmethod hello is broken by wrap_kvs decorator')
+        print('method normal_method is broken by wrap_kvs decorator')
         print(f'{type(e).__name__}: {e}')
         errors.append(e)
 
+    try:
+        MyFiles.hello()
+    except Exception as e:
+        print('classmethod hello is broken by wrap_kvs decorator')
+        print(f'{type(e).__name__}: {e}')
+        errors.append(e)
 
     try:
         MyFiles.hi()
@@ -39,4 +51,3 @@ def test_wrap_kvs_vs_class_and_static_methods():
     if errors:
         first_error, *_ = errors
         raise first_error
-
