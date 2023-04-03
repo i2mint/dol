f99faafd1176fc878aa53a6716650bf946d4acfd
Thor Whalen <thorwhalen1@gmail.com>
2023-04-03 08:58:23 +0200

    chore: move stuff from `dol/core.py`, since not core at all! Delete `core.py` now that empty.

--- dol/paths.py
+++ dol/paths.py
@@ -296,6 +296,24 @@ class PrefixRelativizationMixin:
         return _id[self._prefix_length :]
 
 
+class PrefixRelativization(PrefixRelativizationMixin):
+    """A key wrap that allows one to interface with absolute paths through relative paths.
+    The original intent was for local files. Instead of referencing files through an absolute path such as:
+
+        */A/VERY/LONG/ROOT/FOLDER/the/file/we.want*
+
+    we can instead reference the file as:
+
+        *the/file/we.want*
+
+    But PrefixRelativization can be used, not only for local paths, but when ever a string reference is involved.
+    In fact, not only strings, but any key object that has a __len__, __add__, and subscripting.
+    """
+
+    def __init__(self, _prefix=''):
+        self._prefix = _prefix
+
+
 @store_decorator
 def mk_relative_path_store(
     store_cls=None, *, name=None, with_key_validation=False, prefix_attr='_prefix',
